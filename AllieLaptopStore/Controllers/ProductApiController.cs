a0b724e2aebef944e0ade48db4178156394c5cb1
devalexer <alliemckay.dev@gmail.com>
2017-07-10 19:35:10 -0400

    API working

--- AllieLaptopStore/Controllers/ProductApiController.cs
+++ AllieLaptopStore/Controllers/ProductApiController.cs
@@ -14,45 +14,106 @@ namespace AllieLaptopStore.Controllers
 {
     public class ProductApiController : ApiController
     {
-
-        public static Product GetProduct()
+        [HttpGet]
+        public IHttpActionResult ProductList()
         {
+            // Parses the input (parameters) 
+            
+
+            // Do the logic/Query the Datasource (meat) 
             var url = $"https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=3z6a6dd2jyfjtrpkhdbsaayk&sort=bestSellingRank.asc&show=bestSellingRank,color,customerReviewAverage,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku,thumbnailImage,longDescription&pageSize=100&format=json";
             var request = WebRequest.Create(url);
             var response = request.GetResponse();
             var rawResponse = String.Empty;
 
-            using (var reader = new StreamReader(response.GetResponseStream()))
+            using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8, true, 1024, true))
             {
                 rawResponse = reader.ReadToEnd();
-                //Console.WriteLine(rawResponse);
             }
 
-            var product = JsonConvert.DeserializeObject<Product>(rawResponse);
+            var data = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+           
+            // Format the response 
+            return Ok(data.products.Select(s => new { Name = s.name, Color = s.color }));
 
-            return product;
         }
 
+        public List<Product> ProductListTopTen()
+        {
+            var url = $"https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=3z6a6dd2jyfjtrpkhdbsaayk&sort=bestSellingRank.asc&show=bestSellingRank,color,customerReviewAverage,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku,thumbnailImage,longDescription&pageSize=10&format=json";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var rawResponse = String.Empty;
+
+            using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8, true, 1024, true))
+            {
+                rawResponse = reader.ReadToEnd();
+            }
+
+            var data = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+
+            return data.products;
+
+        }
+
+
         //// GET api/<controller>
-        //public List<Product> Get()
+        //[HttpPost]
+        //public async Task<IHttpActionResult> Index(RootObject ApiData)
+        //{
+        //    var url = $"https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=3z6a6dd2jyfjtrpkhdbsaayk&sort=bestSellingRank.asc&show=bestSellingRank,color,customerReviewAverage,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku,thumbnailImage,longDescription&pageSize=100&format=json";
+        //    var request = WebRequest.Create(url);
+        //    var response = await request.GetResponse();
+        //    var rawResponse = String.Empty;
+
+        //    using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8, true, 1024, true))
+        //    {
+        //        rawResponse = reader.ReadToEnd();
+        //    }
+
+        //    var product = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+        //    //var product = new List<Product>(rawProduct);
+
+        //   return View(product);
+        //}
+
+
+        //public static Product GetProduct()
         //{
         //    var url = $"https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=3z6a6dd2jyfjtrpkhdbsaayk&sort=bestSellingRank.asc&show=bestSellingRank,color,customerReviewAverage,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku,thumbnailImage,longDescription&pageSize=100&format=json";
         //    var request = WebRequest.Create(url);
         //    var response = request.GetResponse();
         //    var rawResponse = String.Empty;
-         
+
         //    using (var reader = new StreamReader(response.GetResponseStream()))
         //    {
         //        rawResponse = reader.ReadToEnd();
-        //        Console.WriteLine(rawResponse);
+        //        //Console.WriteLine(rawResponse);
         //    }
 
-        //    var rawProduct = JsonConvert.DeserializeObject<RootObject>(rawResponse);
-        //    var product = new List<Product>(rawProduct);
+        //    var product = JsonConvert.DeserializeObject<Product>(rawResponse);
 
         //    return product;
         //}
 
+
+        //public static List<Product> GettaProduct()
+        //{
+        //    var url = $"https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=3z6a6dd2jyfjtrpkhdbsaayk&sort=bestSellingRank.asc&show=bestSellingRank,color,customerReviewAverage,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku,thumbnailImage,longDescription&pageSize=100&format=json";
+        //    var request = WebRequest.Create(url);
+        //    var response = request.GetResponse();
+        //    var rawResponse = String.Empty;
+
+        //    using (var reader = new StreamReader(response.GetResponseStream()))
+        //    {
+        //        rawResponse = reader.ReadToEnd();
+        //        //Console.WriteLine(rawResponse);
+        //    }
+
+        //    var product = JsonConvert.DeserializeObject<Product>(rawResponse);
+
+        //    return product;
+        //}
 
     }
 }
